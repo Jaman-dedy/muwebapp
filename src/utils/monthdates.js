1c87145a227b4e3e82edb9b58ef4d2b0aa3d5651
alainburindi <alainburindi62@gmail.com>
2020-08-28 09:31:04 +0200

    translate the DateInput

--- src/utils/monthdates.js
+++ src/utils/monthdates.js
@@ -1,6 +1,18 @@
 const getNumberWithOrdinal = n => {
-  const possiblepostifixes = ['th', 'st', 'nd', 'rd'];
+  const possiblepostifixes = [
+    global.translate('th', 2103),
+    global.translate('st', 2101),
+    global.translate('nd', 2102),
+    global.translate('rd', 2104),
+  ];
   const v = n % 100;
+
+  const language = localStorage.getItem('language');
+
+  if (language === 'fr' && n >= 20) {
+    return n + possiblepostifixes[0];
+  }
+
   return (
     n +
     (possiblepostifixes[(v - 20) % 10] ||
