877afdde741e2732ad9a12768cdb643beea9ec7b
Amani Mpangirwa JdD <jeandedieuam@gmail.com>
2020-10-10 08:58:04 +0000

    fixes

--- src/helpers/replaceCountryFlag.js
+++ src/helpers/replaceCountryFlag.js
@@ -1,10 +1,15 @@
 export default countries => {
   const flags = [{ en: 'us' }];
-  return countries.map(country => {
-    const countryFlag = flags.find(flag => flag[country.flag]);
-    if (countryFlag && countryFlag[country.flag]) {
-      return { ...country, flag: countryFlag[country.flag] };
-    }
-    return country;
-  });
+
+  return countries
+    .map(({ flag, ...rest }) =>
+      flag === 'sw' ? { flag: 'tz', ...rest } : { flag, ...rest },
+    )
+    .map(country => {
+      const countryFlag = flags.find(flag => flag[country.flag]);
+      if (countryFlag && countryFlag[country.flag]) {
+        return { ...country, flag: countryFlag[country.flag] };
+      }
+      return country;
+    });
 };
