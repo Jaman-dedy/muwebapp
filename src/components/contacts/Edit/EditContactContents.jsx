b1aaddd4e0a3ffd4e95b9bb69dbde80b374245c6
Alain Burindi <41288911+alainburindi@users.noreply.github.com>
2020-11-20 12:08:33 +0200

    add image cropper on edit contact (#141)

--- src/components/contacts/Edit/EditContactContents.jsx
+++ src/components/contacts/Edit/EditContactContents.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable react/prop-types */
 import './style.scss';
 
-import cameraIcon from 'assets/images/camera-icon.png';
-import Message from 'components/common/Message';
-import FlatInput from 'components/common/TextField/FlatInput';
-import PhoneNumberInput from 'components/common/TextField/PhoneNumber';
-import Thumbnail from 'components/common/Thumbnail';
-import uploadFile from 'helpers/uploadImages/uploadFile';
 import React, { useEffect, useState } from 'react';
-import updateContactPicture from 'redux/actions/contacts/updateContactPicture';
 import {
   Button,
   Image,
   Modal,
   TransitionablePortal,
 } from 'semantic-ui-react';
-import EditGeneralInfo from 'components/AccountManagement/EmailAndPhone/Edit';
+import cameraIcon from 'assets/images/camera-icon.png';
+import Message from 'components/common/Message';
+import FlatInput from 'components/common/TextField/FlatInput';
+import PhoneNumberInput from 'components/common/TextField/PhoneNumber';
+import Thumbnail from 'components/common/Thumbnail';
+import uploadFile from 'helpers/uploadImages/uploadFile';
+import updateContactPicture from 'redux/actions/contacts/updateContactPicture';
+import ImageCropper from 'components/common/ImageCroper/CropImage';
 
 const EditContactContents = ({
   contact,
@@ -33,6 +33,8 @@ const EditContactContents = ({
   setisEdit,
   isEdit,
 }) => {
+  const [openCropper, setOpenCropper] = useState(false);
+  const [fileToCrop, setFileToCrop] = useState(false);
   const [newContactPicture, setNewContactPicture] = useState(
     contact && contact.PictureURL,
   );
@@ -41,34 +43,38 @@ const EditContactContents = ({
   const [hasError, setHasError] = useState(false);
 
   const onImagePicked = async e => {
-    e.persist();
-    const file = e.target.files;
+    const { files } = e.target;
+    setFileToCrop(files[0]);
+    setOpen(false);
+    setOpenCropper(true);
+  };
 
-    if (file) {
-      setIsUploading(true);
-      const bodyFormData = new FormData();
-      bodyFormData.append('file1', file);
-      const { data } = await uploadFile({ file1: file[0] });
-      if (data && data[0]) {
-        setIsUploading(false);
-        setNewContactPicture(data[0].url);
-        contact.PictureURL = data[0].url;
-        const {
-          PictureURL,
-          CountryCode,
-          FirstName,
-          LastName,
-          PhoneNumber,
-        } = contact;
+  const uploadImage = async file => {
+    setIsUploading(true);
+    setOpenCropper(false);
 
-        updateContactPicture({
-          PictureURL,
-          CountryCode,
-          FirstName,
-          LastName,
-          DestPhoneNum: PhoneNumber,
-        })(dispatch);
-      }
+    const { data } = await uploadFile({ file1: file });
+    if (data && data[0]) {
+      setNewContactPicture(data[0].url);
+      contact.PictureURL = data[0].url;
+      const {
+        PictureURL,
+        CountryCode,
+        FirstName,
+        LastName,
+        PhoneNumber,
+      } = contact;
+
+      updateContactPicture({
+        PictureURL,
+        CountryCode,
+        FirstName,
+        LastName,
+        DestPhoneNum: PhoneNumber,
+      })(dispatch);
+
+      setOpen(true);
+      setIsUploading(false);
     }
   };
 
@@ -79,119 +85,126 @@ const EditContactContents = ({
   }, [newContactPicture]);
 
   return (
-    <TransitionablePortal
-      transition={{
-        duration: 400,
-        animation: 'fade',
-      }}
-      onClose={() => setOpen(false)}
-      open={open}
-    >
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <Modal.Header className="modal-title">
-          {global.translate(`Edit Contact`, 1958)}
-        </Modal.Header>
-        <div className="contents-inner">
-          <div className="avatar-user-image ">
-            <input
-              ref={inputRef}
-              name="files"
-              onChange={onImagePicked}
-              type="file"
-              accept="image/jpeg, image/png"
-              style={{ display: 'none' }}
-            />
+    <>
+      <ImageCropper
+        open={openCropper}
+        setOpen={setOpenCropper}
+        loading={isUploading}
+        file={fileToCrop}
+        uploadImage={uploadImage}
+      />
 
-            <Thumbnail
-              name={contact.FirstName}
-              secondName={contact.LastName}
-              avatar={newContactPicture}
-              size="medium"
-              style={{
-                height: '75px',
-                width: '75px',
-                marginRight: 0,
-                objectFit: 'cover',
-              }}
-              hasError={hasError}
-              setHasError={setHasError}
-            />
+      <TransitionablePortal
+        transition={{
+          duration: 400,
+          animation: 'fade',
+        }}
+        onClose={() => setOpen(false)}
+        open={open}
+      >
+        <Modal open={open} onClose={() => setOpen(false)}>
+          <Modal.Header className="modal-title">
+            {global.translate(`Edit Contact`, 1958)}
+          </Modal.Header>
+          <div className="contents-inner">
+            <div className="avatar-user-image ">
+              <input
+                ref={inputRef}
+                name="files"
+                onChange={onImagePicked}
+                type="file"
+                accept="image/jpeg, image/png"
+                style={{ display: 'none' }}
+              />
 
-            <Image
-              src={cameraIcon}
-              width={18}
-              className="camera-input"
-              disabled={isUploading}
-              onClick={() =>
-                inputRef.current && inputRef.current.click()
-              }
-            />
-            {isUploading && (
-              <small>{global.translate('Working...', 412)}</small>
-            )}
-          </div>
-          <div className="inner-edit-content">
-            <div className="thumb">
-              <div className="first-name">
-                <FlatInput
-                  name="firstName"
-                  id="firstName"
-                  value={editForm.firstName}
-                  placeholder={global.translate('First Name', 8)}
-                  onChange={onEditChange}
-                />
-              </div>
-            </div>
-            <div className="last-name">
-              <FlatInput
-                name="lastName"
-                placeholder={global.translate('Last Name', 9)}
-                value={editForm.lastName}
-                onChange={onEditChange}
+              <Thumbnail
+                name={contact.FirstName}
+                secondName={contact.LastName}
+                avatar={newContactPicture}
+                size="medium"
+                style={{
+                  height: '75px',
+                  width: '75px',
+                  marginRight: 0,
+                  objectFit: 'cover',
+                }}
+                hasError={hasError}
+                setHasError={setHasError}
+              />
+
+              <Image
+                src={cameraIcon}
+                width={18}
+                className="camera-input"
+                disabled={isUploading}
+                onClick={() =>
+                  inputRef.current && inputRef.current.click()
+                }
               />
             </div>
-            <div className="phone-section">
-              <div className="area">
-                <PhoneNumberInput
-                  name="phoneNumber"
-                  value={editForm.phoneNumber}
-                  placeholder={global.translate('Phone number', 13)}
-                  country={country}
-                  setCountry={setCountry}
+            <div className="inner-edit-content">
+              <div className="thumb">
+                <div className="first-name">
+                  <FlatInput
+                    name="firstName"
+                    id="firstName"
+                    value={editForm.firstName}
+                    placeholder={global.translate('First Name', 8)}
+                    onChange={onEditChange}
+                  />
+                </div>
+              </div>
+              <div className="last-name">
+                <FlatInput
+                  name="lastName"
+                  placeholder={global.translate('Last Name', 9)}
+                  value={editForm.lastName}
                   onChange={onEditChange}
                 />
               </div>
+              <div className="phone-section">
+                <div className="area">
+                  <PhoneNumberInput
+                    name="phoneNumber"
+                    value={editForm.phoneNumber}
+                    placeholder={global.translate('Phone number', 13)}
+                    country={country}
+                    setCountry={setCountry}
+                    onChange={onEditChange}
+                  />
+                </div>
+              </div>
             </div>
+            {editErrors && <Message error message={editErrors} />}
           </div>
-          {editErrors && <Message error message={editErrors} />}
-        </div>
-        <Modal.Actions>
-          <Button
-            disabled={loading}
-            basic
-            color="red"
-            onClick={() => {
-              setIsUploading(false);
-              setEditErrors(null);
-              setisEdit(!isEdit);
-              setNewContactPicture('');
-            }}
-          >
-            {global.translate('Cancel', 1602)}
-          </Button>
-          <Button
-            disabled={loading}
-            loading={loading}
-            onClick={() => {
-              handleEditInfo('external', contact);
-            }}
-            positive
-          >
-            {global.translate('Save', 614)}
-          </Button>
-        </Modal.Actions>
-      </Modal>
-    </TransitionablePortal>
+          <Modal.Actions>
+            <Button
+              disabled={loading}
+              basic
+              color="red"
+              onClick={() => {
+                setIsUploading(false);
+                setEditErrors(null);
+                setisEdit(!isEdit);
+                setNewContactPicture('');
+              }}
+            >
+              {global.translate('Cancel', 1602)}
+            </Button>
+            <Button
+              disabled={loading}
+              loading={loading}
+              onClick={() => {
+                handleEditInfo('external', contact);
+              }}
+              positive
+            >
+              {global.translate('Save', 614)}
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </TransitionablePortal>
+    </>
   );
 };
 
