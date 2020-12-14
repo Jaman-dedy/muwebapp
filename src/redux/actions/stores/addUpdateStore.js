b3ed104a4a18a743a9fbdb90d43fd21d649802af
Karl MUSINGO <35597858+Karlmusingo@users.noreply.github.com>
2020-12-14 18:08:49 +0200

    dispatch the image urls of the logo and banner from the response when they were not updated (#177)
    Co-authored-by: Karl Musingo <admin@MacBook-Pro-de-ADMIN.local>

--- src/redux/actions/stores/addUpdateStore.js
+++ src/redux/actions/stores/addUpdateStore.js
@@ -19,6 +19,14 @@ export default (requestData, isEditing) => dispatch => {
           type: ADD_UPDATE_STORE_START,
         }),
       onSuccess: data => dispatch => {
+        if (data[0].Result !== 'Success') {
+          return dispatch({
+            type: ADD_UPDATE_STORE_ERROR,
+            payload: {
+              ...data[0],
+            },
+          });
+        }
         toast.success(
           global.translate('Your store data is saved.', 913),
         );
@@ -26,8 +34,9 @@ export default (requestData, isEditing) => dispatch => {
           type: ADD_UPDATE_STORE_SUCCESS,
           payload: {
             ...data[0],
-            StoreBanner: requestData?.BannerURL,
-            StoreLogo: requestData?.LogoURL,
+            StoreBanner:
+              requestData?.BannerURL || data[0]?.StoreBanner,
+            StoreLogo: requestData?.LogoURL || data[0]?.StoreLogo,
             success: data[0].Result === 'Success',
             isEditing,
           },
