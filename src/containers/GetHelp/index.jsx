fe0c009a7cfa606a580ce6965f2a8cec17b411ee
Karl MUSINGO <35597858+Karlmusingo@users.noreply.github.com>
2021-12-02 15:25:22 +0200

    Remove double notification when the user sends an email for help (#437)
    * Remove double notification when the user sends an email for help
    
    * Remove the scrollbar on the text box

--- src/containers/GetHelp/index.jsx
+++ src/containers/GetHelp/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { toast } from 'react-toastify';
 import submitEmail, { clearSendEmail } from 'redux/actions/sendEmail';
 import GetHelp from 'components/GetHelp';
 
@@ -8,7 +7,6 @@ const GetHelpContainer = () => {
   const dispatch = useDispatch();
   const [text, setText] = useState('');
   const [isSent, setIsSent] = useState(false);
-  const [toastMessage, setToastMessage] = useState(null);
   const [disable, setDisable] = useState(true);
   const [name, setName] = useState('');
   const { userData } = useSelector(({ user }) => user);
@@ -32,43 +30,21 @@ const GetHelpContainer = () => {
 
   const submitText = () => {
     const data = {
-      To: 'jeandedieuam@gmail.com',
+      To: process.env.REACT_APP_SUPPORT_EMAIL || '',
       Subject: `Email from ${name}`,
-      HTML: newText.split('\n').join(''),
+      Email: newText.split('\n').join(''),
+      PID: userData?.data?.PID,
+      Category: '1',
     };
     submitEmail(data)(dispatch);
   };
+
   useEffect(() => {
-    if (sendEmail.data) {
-      setToastMessage(sendEmail?.data?.Description);
+    if (sendEmail.data || sendEmail.error) {
       setIsSent(true);
+      clearSendEmail()(dispatch);
     }
   }, [sendEmail]);
-
-  useEffect(() => {
-    if (toastMessage && sendEmail.data) {
-      toast.success(toastMessage);
-      clearSendEmail()(dispatch);
-      setToastMessage(null);
-      setText('');
-    }
-  }, [toastMessage]);
-
-  useEffect(() => {
-    if (sendEmail.error) {
-      setToastMessage(sendEmail?.error?.Description);
-      setIsSent(true);
-    }
-  }, [sendEmail]);
-
-  useEffect(() => {
-    if (toastMessage && sendEmail.error) {
-      toast.error(toastMessage);
-      clearSendEmail()(dispatch);
-      setToastMessage(null);
-      setText('');
-    }
-  }, [toastMessage]);
 
   return (
     <GetHelp
